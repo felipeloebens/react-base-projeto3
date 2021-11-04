9ff1ccbc1b2e93da91444e005c8645573d45e9a1
FelipeL <felipe@fockink.ind.br>
2021-11-04 16:40:28 -0300

    criacao do formulario base

--- react-base-projeto3/conversor-de-moedas/src/conversor-moedas.js
+++ react-base-projeto3/conversor-de-moedas/src/conversor-moedas.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import './conversor-moedas.css';
+import { Button, Form, Col, Row, Spinner } from 'react-bootstrap';
+import './conversor-moedas.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 function ConversorMoedas() {
   return (
+  <div class="jumbotron">
    <h1> ConversorMoedas </h1>
+   <Form>
+     <Row>
+       <Col sm="3">
+        <Form.Control 
+          placeholder="0" 
+          value={1}
+          required />
+       </Col>
+       <Col sm="3">
+         <Form.Select>
+         </Form.Select>
+       </Col>
+       <Col sm="1" className="text-center" style={{paddingTop: '5px'}}>
+          <FontAwesomeIcon icon={faAngleDoubleRight}/>
+       </Col>
+       <Col sm="3">
+         <Form.Select>
+         </Form.Select>       
+       </Col>
+       <Col sm="2">
+        <Button variant="success" type="submit" >
+          <Spinner animation="border" size="sm" />
+          Converter 
+        </Button>
+       </Col>
+     </Row>
+   </Form>
+  </div>
   );
 }
 
