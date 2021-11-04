06e9c3d2ab9e86f68087c43369b3302b0db7c052
FelipeL <felipe@fockink.ind.br>
2021-11-04 09:57:50 -0300

    inserção das bibliotecas e componente principal

---
+++ react-base-projeto3/conversor-de-moedas/src/conversor-moedas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './conversor-moedas.css';
+
+function ConversorMoedas() {
+  return (
+   <h1> ConversorMoedas </h1>
+  );
+}
+
+export default ConversorMoedas;
---
+++ react-base-projeto3/conversor-de-moedas/src/conversor-moedas.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import ConversorMoedas from './conversor-moedas';
+
+test('deve renderizar o componente sem erros', () => {
+  render(<ConversorMoedas />);
+
+});
