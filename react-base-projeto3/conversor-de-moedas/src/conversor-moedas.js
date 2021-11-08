de3130d2315ed89b63c6b2be81a1bd269303ae35
FelipeL <felipe@fockink.ind.br>
2021-11-08 16:11:18 -0300

    termino do componente de lsitagem de moedas, e comeco da implementacao do formulario de conversao

--- react-base-projeto3/conversor-de-moedas/src/conversor-moedas.js
+++ react-base-projeto3/conversor-de-moedas/src/conversor-moedas.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './conversor-moedas.css';
 import { Button, Form, Col, Row, Spinner, Alert, Modal } from 'react-bootstrap';
 import './conversor-moedas.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+import ListarMoedas from './listar-moedas';
 
 function ConversorMoedas() {
+  const [valor, setValor] = useState('1');
+  const [moedaDe, setMoedaDe] = useState('BRL');
+  const [moedaPara, setMoedaPara] = useState('USD');
+
+  function handleValor(event) {
+    setValor(event.target.value.replace(/\D/g, ''));
+  }
+
+  function handleMoedaDe(event) {
+    setMoedaDe(event.target.value);
+  }
+
+  function handleMoedaPara(event) {
+    setMoedaPara(event.target.value);
+  }
+
   return (
   <div class="jumbotron">
    <h1> ConversorMoedas </h1>
@@ -17,18 +34,25 @@ function ConversorMoedas() {
        <Col sm="3">
         <Form.Control 
           placeholder="0" 
-          value={1}
+          value={valor}
+          onChange={handleValor}
           required />
        </Col>
        <Col sm="3">
-         <Form.Select>
+         <Form.Select 
+         value={moedaDe}
+         onChange={handleMoedaDe}>
+            <ListarMoedas />
          </Form.Select>
        </Col>
        <Col sm="1" className="text-center" style={{paddingTop: '5px'}}>
           <FontAwesomeIcon icon={faAngleDoubleRight}/>
        </Col>
        <Col sm="3">
-         <Form.Select>
+         <Form.Select
+          value={moedaPara}
+          onChange={handleMoedaPara}>
+          <ListarMoedas />
          </Form.Select>       
        </Col>
        <Col sm="2">
@@ -39,7 +63,7 @@ function ConversorMoedas() {
        </Col>
      </Row>
    </Form>
-   <Modal show={true}>
+   <Modal show={false}>
     <Modal.Header closeButton>
       <Modal.Title> Conversão</Modal.Title>
     </Modal.Header>
